bc3c4ca02bd691c866d4433305146a2de1e866a9
shark820 <shark820@naver.com>
2024-05-24 01:25:00 +0900

    EditedSettingScreen

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dashboard_rpm/providers/auth_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
-
-bool Twenty_Four_Hours_Rule=true;
+bool twentyFourHoursRule=true;
+const double settingValueTextSize=20;
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -17,46 +19,82 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           leading:IconButton(onPressed:(){}, icon: Icon(Icons.arrow_left_outlined),color: Colors.black,),
-          title:Text("설정",style:TextStyle(fontSize:30, color:Colors.black,),),
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(15, 25, 15, 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //닉네임
-            Container(
-              child:Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children:[
-                  Text('닉네임',style:TextStyle(fontSize:15, color:Colors.black),),
-                  TextButton(onPressed:(){},child:Text("TEXT BUTTON"),),
+          title:Text("설정",style:TextStyle(fontSize:27, color:Colors.black,),),
+        ),
+        body: Container(
+            padding: EdgeInsets.fromLTRB(15, 25, 15, 10),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //닉네임
+                  Container(
+                      child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('닉네임',style:TextStyle(fontSize:settingValueTextSize, color:Colors.black),),
+                            //TextButton(onPressed:(){},child:Text(),),
+                            /*StreamBuilder(
+                        stream: FirebaseFirestore.instance.collection('user').snapshots(),
+                        builder: (context,AsyncSnapshot<QuerySnapshot<Map<String,dynamic>>> snapshot) {
+                          if(snapshot.hasError){
+                            return Text(
+                              '...',style:TextStyle(fontSize:15,color: Colors.grey),
+                            );
+                          }
+                          itemCount:snapshot.data!.docs.length;
+                          itemBuilder:(context, index) {
+                          return Text(
+                            snapshot.data!.docs[index]['nickname'],
+                            style: TextStyle(fontSize: 15, color: Colors.grey),
+                          );
+                        };
+                      }),*/
+                          ]
+                      )
+                  ),
+                  //24시간으로 표현
+                  Container(
+                      child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                            Text('24시간으로 표현',style:TextStyle(fontSize:settingValueTextSize, color:Colors.black),),
+
+                            CupertinoSwitch(
+                              value: twentyFourHoursRule,
+                              activeColor: CupertinoColors.activeBlue,
+                              onChanged: (bool? value){
+                                setState(() {
+                                  twentyFourHoursRule = value ?? false;
+                                });
+                              },
+                            ),
+                          ]
+                      )
+                  ),
+                  //일정 알림
+                  Container(
+                      child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                            Text('일정 알림',style:TextStyle(fontSize:settingValueTextSize, color:Colors.black),),
+                          ]
+                      )
+                  ),
+                  //카테고리 설정
+                  Container(
+                      child:Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children:[
+                            Text('카테고리 설정',style:TextStyle(fontSize:settingValueTextSize, color:Colors.black),),
+                            IconButton(onPressed:(){}, icon: Icon(Icons.add_circle),color: Colors.blue,),
+                          ]
+                      )
+                  ),
                 ]
-              )
-            ),
-            //24시간으로 표현
-            Container(
-                child:Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:[
-                      Text('24시간으로 표현',style:TextStyle(fontSize:15, color:Colors.black),),
-                      CupertinoSwitch(
-                          value: Twenty_Four_Hours_Rule,
-                          activeColor: CupertinoColors.activeBlue,
-                          onChanged: (bool? value){
-                            setState(() {
-                              Twenty_Four_Hours_Rule = value ?? false;
-                            });
-                          },
-                      ),
-                    ]
-                )
-            ),
-          ]
+            )
         )
-      )
     );
   }
 }
