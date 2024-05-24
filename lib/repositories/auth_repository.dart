31403ebf9bdd56dbfa2c0604817ba851fbb23d89
leejun00416 <ljy00416@gmail.com>
2024-05-24 12:23:08 +0900

    구글 로그인 구현

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -3,6 +3,7 @@ import 'package:dashboard_rpm/exceptions/custom_exception.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+
 class AuthRepository {
   final FirebaseAuth firebaseAuth;
   final FirebaseStorage firebaseStorage;
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -4,9 +4,12 @@ import 'package:dashboard_rpm/providers/auth_state.dart';
 import 'package:dashboard_rpm/screens/signup_screen.dart';
 import 'package:dashboard_rpm/utils/logger.dart';
 import 'package:dashboard_rpm/widgets/error_dialog_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:validators/validators.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
@@ -158,6 +161,36 @@ class _SigninScreenState extends State<SigninScreen> {
                         style: TextStyle(fontSize: 20),
                       ),
                     ),
+
+                  Expanded (
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                        onTap: () {
+                          signInWithGoogle();
+                        },
+                        child: Card(
+                          margin: const EdgeInsets.fromLTRB (20, 20, 20, 0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular (7)),
+                          elevation: 2,
+                          child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              Image.asset('android_light_rd_na@1x.png'),
+                              const SizedBox(
+                               width: 10,
+                              ), // SizedBox
+                              const Text("Sign In With Google",
+                                style:
+                                  TextStyle(color: Colors.grey, fontSize: 17))
+                              ]),
+                        ),
+                      ) // Inkwell
+
+                  ])) // Column // Expanded
+
                   ].reversed.toList(),
                 ),
               ),
@@ -166,5 +199,21 @@ class _SigninScreenState extends State<SigninScreen> {
         ),
       ),
     );
+  }
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
