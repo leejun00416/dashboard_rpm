263bfab6a705cedbe06f6ade4dcbc175d2c64ca8
BOOK714\canny <cannyel@naver.com>
2024-05-29 03:12:27 +0900

    메인화면 디자인 수정, 하단 상태창 개발.
    마찬가지로 main_screen.dart에 그대로 복붙하면 작동되는 것 같긴 함...

--- lib/screens/today_screen.dart
+++ lib/screens/today_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key); // key 매개변수 수정
@@ -10,7 +12,9 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> { // _MainScreenState 클래스 추가
   @override
   Widget build(BuildContext context) {
-    return SchedulePage();
+    return MaterialApp(
+        home: SchedulePage(),
+    );
   }
 }
 
@@ -23,8 +27,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SchedulePage extends StatefulWidget {
+  @override
+  _SchedulePageState createState() => _SchedulePageState();
+}
 
-class SchedulePage extends StatelessWidget {
+class _SchedulePageState extends State<SchedulePage> {
+  bool _showNotesSection = false;
+
+  void _toggleNotesSection() {
+    setState(() {
+      _showNotesSection = !_showNotesSection;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +49,7 @@ class SchedulePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
-              icon: Icon(Icons.arrow_back),
+              icon: Icon(Icons.keyboard_arrow_left),
               onPressed: () {},
             ),
             TextButton(
@@ -55,10 +71,10 @@ class SchedulePage extends StatelessWidget {
                   },
                 );
               },
-              child: Text('5/7 (화)', style: TextStyle(color: Colors.white)),
+              child: Text('5/7 (화)', style: TextStyle(color: Colors.black)),
             ),
             IconButton(
-              icon: Icon(Icons.arrow_forward),
+              icon: Icon(Icons.keyboard_arrow_right),
               onPressed: () {},
             ),
           ],
@@ -67,8 +83,9 @@ class SchedulePage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(child: ScheduleGrid()),
-          NotesSection(),
-          PlannerButtons(),
+          if (_showNotesSection) NotesSection(),
+          PlannerActionBar1(toggleNotesSection: _toggleNotesSection),
+          PlannerActionBar2(),
         ],
       ),
     );
@@ -79,68 +96,70 @@ class ScheduleGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(child: buildLeftTimeSlots()),
-              buildMiddleTimeline(),
-              Expanded(child: buildRightTimeSlots()),
-            ],
-          ),
-        ],
+      child: Container(
+        //padding: EdgeInsets.fromLTRB(10,0,10,0),
+        color: Colors.grey,
+        child:
+        Row(
+          children: [
+            Expanded(child: buildLeftTimeSlots(), flex: 6,),
+            Expanded(child: buildMiddleTimeline(), flex: 1,),
+            Expanded(child: buildRightTimeSlots(), flex: 6,),
+          ],
+        ),
       ),
     );
   }
 
   Widget buildLeftTimeSlots() {
     return GridView.builder(
-      padding: EdgeInsets.fromLTRB(10,0,0,0),
+      //padding: EdgeInsets.fromLTRB(10,0,0,0),
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 6, // 가로로 여섯 개의 칸
-        mainAxisSpacing: 3.0,
+        mainAxisSpacing: 1.0,
         crossAxisSpacing: 1.0,
         childAspectRatio: 1, // 높이/너비 비율
       ),
       itemCount: 144, // 오전 12시간
       itemBuilder: (context, index) {
-        return TimeSlot('$index', Colors.brown, index-60, index + 1);
+        return TimeSlot(' ', Colors.white, index-60, index + 1);
       },
     );
   }
 
   Widget buildMiddleTimeline() {
-    return Container(
-      width: 30, // 타임라인의 너비를 조정합니다.
-      color: Colors.grey[300],
-      child: Column(
-        children: List.generate(24, (index) {
-          return Container(
-            height: 40.0,
-            alignment: Alignment.center,
-            child: Text('$index', style: TextStyle(fontSize: 12.0)),
-          );
-        }),
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1, // 가로로 여섯 개의 칸
+        mainAxisSpacing: 0.0,
+        crossAxisSpacing: 0.0,
+        childAspectRatio: 1, // 높이/너비 비율
       ),
+      itemCount: 24, // 오전 12시간
+      itemBuilder: (context, index) {
+        return TimeSlot('$index', Colors.white38, index, index);
+      },
     );
   }
 
   Widget buildRightTimeSlots() {
     return GridView.builder(
-      padding: EdgeInsets.all(8.0),
+      //padding: EdgeInsets.fromLTRB(0,0,10,0),
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 6, // 가로로 두 개의 칸
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
         childAspectRatio: 1, // 높이/너비 비율
       ),
-      itemCount: 72, // 오후 12시간
+      itemCount: 144, // 오후 12시간
       itemBuilder: (context, index) {
-        return TimeSlot(" $index", Colors.greenAccent, index, index +1);
+        return TimeSlot(" ", Colors.white, index, index +1);
       },
     );
   }
@@ -159,7 +178,7 @@ class TimeSlot extends StatelessWidget {
     return Container(
       color: color,
       alignment: Alignment.center,
-      child: Text(label, style: TextStyle(fontSize: 16.0, color: Colors.white)),
+      child: Text(label, style: TextStyle(fontSize: 16.0, color: Colors.black, fontWeight: FontWeight.bold,)),
     );
   }
 }
@@ -167,37 +186,116 @@ class TimeSlot extends StatelessWidget {
 class NotesSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Expanded(child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[200],
+      ),
+      margin: EdgeInsets.all(16.0),
       padding: EdgeInsets.all(16.0),
-      color: Colors.grey[200],
+
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('화분에 물 주기', style: TextStyle(fontSize: 16.0)),
-          Text('rpm 39페이지까지 풀기', style: TextStyle(fontSize: 16.0)),
+          Text('ㅁ 화분에 물 주기', style: TextStyle(fontSize: 16.0)),
+          Text('ㅁ rpm 39페이지까지 풀기', style: TextStyle(fontSize: 16.0)),
           Text('이번 주에는 꼭 운동가자', style: TextStyle(fontSize: 16.0)),
+        ],
+      ),
+    ),);
+  }
+}
+
+class PlannerActionBar1 extends StatelessWidget {
+  final VoidCallback toggleNotesSection;
+
+  PlannerActionBar1({required this.toggleNotesSection});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      color: Colors.amberAccent,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('계획:', style: TextStyle(fontSize: 16.0)),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('소프1', style: TextStyle(fontSize: 16.0)),
+            style: OutlinedButton.styleFrom(
+              backgroundColor: Colors.black87,
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(7),
+                ),
+              ),
+            ),
+          ),
+          Text('할 시간', style: TextStyle(fontSize: 16.0)),
+          IconButton(
+            icon: Icon(Icons.format_list_bulleted),
+            onPressed: toggleNotesSection,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.black12),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class PlannerButtons extends StatelessWidget {
+class PlannerActionBar2 extends StatefulWidget {
+  @override
+  _PlannerActionBar2State createState() => _PlannerActionBar2State();
+}
+
+class _PlannerActionBar2State extends State<PlannerActionBar2> {
+  final List<String> _dropdownItems = ['소프1', '공기수', '잉컨', '플러터'];
+  String _selectedItem = '소프1';
+  bool _isPlaying = false;
+
+  void _togglePlayPause() {
+    setState(() {
+      _isPlaying = !_isPlaying;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8.0),
-      color: Colors.yellow[100],
+      color: const Color(0xffefefeb),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('계획: 소프'),
+          Text(' 기록:', style: TextStyle(fontSize: 16.0)),
+          DropdownButton<String>(
+            value: _selectedItem,
+            style: TextStyle(
+              color: Colors.red,
+            ),
+            items: _dropdownItems.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                _selectedItem = newValue!;
+              });
+            },
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('기록: 공기수'),
+          Text('하는 중', style: TextStyle(fontSize: 16.0)),
+          IconButton(
+            icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+            onPressed: _togglePlayPause,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.black12),
+            ),
           ),
         ],
       ),
